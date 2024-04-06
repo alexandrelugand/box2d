5869e7412dade7291b394d636ccaca9571e251c0
Alexandre Lugand <alexandre.lugand@gmail.com>
2024-04-07 00:48:03 +0200

    feat: Scripting engine using Mono for Desktop and .Net 8.0 and Blazor for Web Assembly

--- premake5.lua
+++ premake5.lua
@@ -1,44 +1,66 @@
-project "Box2D"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++20"
-	staticruntime "off"
-	architecture "x64"
-	targetdir ("%{wks.location}/bin/" .. outputdir)
-	objdir ("%{wks.location}/bin-int/" .. tmpdir)
+local _files =
+{
+	"src/**.h",
+	"src/**.cpp",
+	"include/**.h"
+}
 
-	files
-	{
-		"src/**.h",
-		"src/**.cpp",
-		"include/**.h"
-	}
+local _includedirs =
+{
+	"include",
+	"src"
+}
 
-	includedirs
-	{
-		"include",
-		"src"
-	}
+group "Dependencies"
+	project "Box2D"
+		kind "StaticLib"
+		language "C++"
+		cppdialect "C++20"
+		staticruntime "off"
+		architecture "x64"
+		targetdir ("%{wks.location}/bin/" .. outputdir)
+		objdir ("%{wks.location}/bin-int/" .. tmpdir)
 
-	filter "system:windows"
-		systemversion "latest"
+		files(_files)
+		includedirs(_includedirs)
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-		optimize "Off"
+		filter "system:windows"
+			systemversion "latest"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "full"
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+			optimize "Off"
 
-	filter { "action:gmake2" }
-		architecture "x86"
-		makesettings [[
-CC = emcc
-CXX = em++
-AR = emar
-		]]
-		targetextension  ".a"
-		buildoptions { "-Wall -Wformat -s DISABLE_EXCEPTION_CATCHING=1" }
-		linkoptions { "-s DISABLE_EXCEPTION_CATCHING=1 -s USE_SDL=2 -s USE_GLFW=3 -s FULL_ES3=1 -s FORCE_FILESYSTEM=1 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -s ASSERTIONS=1 --no-heap-copy --preload-file ./misc/fonts@/fonts" }
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "full"
+
+group "Browser/Dependencies"
+	project "Box2D.Web"
+		kind "StaticLib"
+		targetname "Box2D"
+		targetextension ".a"
+		language "C++"
+		cppdialect "C++20"
+		staticruntime "off"
+		architecture "x64"
+		targetdir ("%{wks.location}/bin/" .. outputdirweb)
+		objdir ("%{wks.location}/bin-int/" .. tmpdirweb)
+
+		files(_files)
+		includedirs(_includedirs)
+
+		filter "system:windows"
+			systemversion "latest"
+
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+			optimize "Off"
+
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "full"
+
+group ""
